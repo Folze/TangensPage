0a24cb45d41a72516d9f87e4ea2dc2691bed7279
Folze <thefolze@gmail.com>
2022-05-24 23:12:49 +0500

    remade linksbuttons

--- src/MainPage/Navbar/Navbar.jsx
+++ src/MainPage/Navbar/Navbar.jsx
@@ -6,49 +6,47 @@ import styles from './Navbar.module.scss'
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
-            <div className={styles.header}>
-                {/* <div className={styles.logo}> */}
-                {/* <img src='/' /> */}
-                {/* </div> */}
+            {/* <div className={styles.logo}> */}
+            {/* <img src='/' /> */}
+            {/* </div> */}
 
-                {/* 1 кнопка */}
-                <div className={styles.button}>
-                    <div>
-                        <button className={styles.buttonFour}>
-                            <span></span><span></span><span></span><span></span>
-                            Главная
-                        </button>
-                    </div>
-                    {/* 2 кнока */}
-
-                    <div>
-                        <NavLink to="/Blog">
-                            <button className={styles.buttonFour}>
-                                <span></span><span></span><span></span><span></span>
-                                Blog
-                            </button>
-                        </NavLink>
-                    </div>
-
-                    {/* 3 Кнопка */}
-
-                    <NavLink to="/Games">
-                        <button className={styles.buttonFour}>
-                            <span></span><span></span><span></span><span></span>
-                            Игры
-                        </button>
-                    </NavLink>
-
-                    {/* 4 кнопка */}
-
-                    <NavLink to="/Information">
-                        <button className={styles.buttonFour}>
-                            <span></span><span></span><span></span><span></span>
-                            Информация
-                        </button>
-                    </NavLink>
-
+            {/* 1 кнопка */}
+            <div className={styles.button}>
+                <div>
+                    <button className={styles.buttonFour}>
+                        <span></span><span></span><span></span><span></span>
+                        Главная
+                    </button>
                 </div>
+                {/* 2 кнока */}
+
+                <div>
+                    <NavLink to="/Blog">
+                        <button className={styles.buttonFour}>
+                            <span></span><span></span><span></span><span></span>
+                            Blog
+                        </button>
+                    </NavLink>
+                </div>
+
+                {/* 3 Кнопка */}
+
+                <NavLink to="/Games">
+                    <button className={styles.buttonFour}>
+                        <span></span><span></span><span></span><span></span>
+                        Игры
+                    </button>
+                </NavLink>
+
+                {/* 4 кнопка */}
+
+                <NavLink to="/Information">
+                    <button className={styles.buttonFour}>
+                        <span></span><span></span><span></span><span></span>
+                        Информация
+                    </button>
+                </NavLink>
+
             </div>
         </div>
     )
